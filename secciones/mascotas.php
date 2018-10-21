fb1d5ae8d27f66784174cc686f23474e192d7598
Lautaro <lauthuichalaf@gmail.com>
2018-10-21 01:26:38 -0300

    Modifiqué como se van a ver las noticias de perros perdidos o encontrados

--- secciones/mascotas.php
+++ secciones/mascotas.php
@@ -12,72 +12,72 @@
               <a class="btn btn-primary btn-block">Me perdí</a>
             </div>
             <div class="col">
-              <a class="btn btn-outline-success btn-block">Me encontraron</a>
+              <a class="btn btn-success btn-block">Me encontraron</a>
             </div>
           </div>
 <br>
 			<article>
-				<div class="card text-center alert alert-primary">
+				<div class="card text-center alert bg-primary">
 					<img class="" src="images/perdido.jpg" alt="">
 					<br>
 					<p>Se escapó anoche en el kilómetro 12600, lo vieron por la ruta parada km13 aquiles, pitbull marrón, con collar rojo. Cualquier cosa comunicarse a 2944332727</p>
 						<div class="card-body">
-							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+							<a href="#" class="btn btn-primary alert alert-light">Contactar Usuario</a>
 						</div>
 				</div>
 		  </article>
 
 			<article>
-				<div class="card text-center alert alert-primary">
+				<div class="card text-center alert bg-primary">
 					<img class="" src="images/perdido2.jpg" alt="">
 					<br>
 					<p>Perro adulto mayor con dificultad para caminar en onelli y 25 de Mayo</p>
 						<div class="card-body">
-							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+							<a href="#" class="btn btn-primary alert alert-light">Contactar Usuario</a>
 						</div>
 				</div>
       </article>
 
 			<article>
-				<div class="card text-center alert alert-primary">
+				<div class="card text-center alert bg-primary">
 					<img class="" src="images/perdido3.jpg" alt="">
 					<br>
 					<p>Se perdió en  villa lago gutierrez llamar al 2944920440</p>
 						<div class="card-body">
-							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+							<a href="#" class="btn btn-primary alert alert-light">Contactar Usuario</a>
 						</div>
 				</div>
       </article>
 
 			<article>
-				<div class="card text-center alert alert-primary">
+				<div class="card text-center alert bg-success">
 					<img class="" src="images/perdido4.jpg" alt="">
 					<br>
 					<p>Está en la zona de gallardo y Rivadavia. Se nota que está perdido!!</p>
 						<div class="card-body">
-							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+							<a href="#" class="btn btn-success alert alert-light">Contactar Usuario</a>
 						</div>
 				</div>
       </article>
 
 			<article>
-				<div class="card text-center alert alert-primary">
+				<div class="card text-center alert bg-success">
 					<img class="" src="images/perdido5.jpg" alt="">
 					<br>
 					<p>Se extravió caniche macho sin cola con collar azul en San Francisco 3 agradecere info</p>
 						<div class="card-body">
-							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+							<a href="#" class="btn btn-success alert alert-light">Contactar Usuario</a>
 						</div>
 				</div>
 			</article>
 
 			<article>
-				<div class="card text-center alert alert-primary">
+				<div class="card text-center alert bg-success">
 					<img class="" src="images/perdido6.jpg" alt="">
 					<br>
 					<p>Está este perro en la entrada a las victorias con colla verde</p>
 						<div class="card-body">
-							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+							<a href="#" class="btn btn-success alert alert-light">Contactar Usuario</a>
 						</div>
 				</div>
 			</article>
