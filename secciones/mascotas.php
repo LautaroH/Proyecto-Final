4f7f7ed2c4d3826303c78cc84252e6c912849b9b
Lautaro <lauthuichalaf@gmail.com>
2018-10-21 00:31:26 -0300

    Cambio la vista de las noticias

--- secciones/mascotas.php
+++ secciones/mascotas.php
@@ -11,86 +11,75 @@
             <div class="col">
               <a class="btn btn-primary btn-block">Me perdí</a>
             </div>
-
             <div class="col">
               <a class="btn btn-outline-success btn-block">Me encontraron</a>
             </div>
           </div>
-          
-          <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Cras justo odio</li>
-              <li class="list-group-item">Dapibus ac facilisis in</li>
-              <li class="list-group-item">Vestibulum at eros</li>
-            </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>
-
-          <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="images/perdido.jpg" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text" style="color: black">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-
-
-
-            <article>
-                <div class="alert alert-primary">
-              <h1><a href="the100.html"></a></h1>
-              <a href="the100.html"><img src="images/perdido.jpg" alt="" /></a>
-              <p>Se escapó anoche en el kilómetro 12600, lo vieron por la ruta parada km13 aquiles, pitbull marrón, con collar rojo. Cualquier cosa comunicarse a 2944332727</p>
-
-              </div>
-              <a href="#" class="btn btn-primary alert alert-primary">Go somewhere</a>
-            </article>
-
-
-
-          <article>
-
-            <h1><a href="arrow.html"></a></h1>
-            <a href="arrow.html"><img src="images/perdido2.jpg" alt="" /></a>
-            <p>Perro adulto mayor con dificultad para caminar en onelli y 25 de Mayo</p>
-
-
-          </article>
-          <article class="lastarticle">
-
-            <h1><a href="strangerthings.html"></a></h1>
-            <a href="StrangerThings.html"><img src="images/perdido3.jpg" alt="" /></a>
-            <p>Se perdió en  villa lago gutierrez llamar al 2944920440</p>
-
-          </article>
-          <article>
-
-            <h1><a href="the100.html"></a></h1>
-            <a href="the100.html"><img src="images/perdido4.jpg" alt="" /></a>
-            <p>Está en la zona de gallardo y Rivadavia. Se nota que está perdido!!</p>
-          </article>
-          <article>
-
-            <h1><a href="arrow.html"></a></h1>
-            <a href="arrow.html"><img src="images/perdido5.jpg" alt="" /></a>
-            <p>Se extravió caniche macho sin cola con collar azul en San Francisco 3 agradecere info</p>
-
-          </article>
-          <article class="lastarticle">
-
-            <h1><a href="strangerthings.html"></a></h1>
-            <a href="StrangerThings.html"><img src="images/perdido6.jpg" alt="" /></a>
-            <p>Está este perro en la entrada a las victorias con colla verde</p>
-
-          </article>
+<br>
+			<article>
+				<div class="card text-center alert alert-primary">
+					<img class="" src="images/perdido.jpg" alt="">
+					<br>
+					<p>Se escapó anoche en el kilómetro 12600, lo vieron por la ruta parada km13 aquiles, pitbull marrón, con collar rojo. Cualquier cosa comunicarse a 2944332727</p>
+						<div class="card-body">
+							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+						</div>
 				</div>
-			</section>
+		  </article>
+
+			<article>
+				<div class="card text-center alert alert-primary">
+					<img class="" src="images/perdido2.jpg" alt="">
+					<br>
+					<p>Perro adulto mayor con dificultad para caminar en onelli y 25 de Mayo</p>
+						<div class="card-body">
+							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+						</div>
+				</div>
+      </article>
+
+			<article>
+				<div class="card text-center alert alert-primary">
+					<img class="" src="images/perdido3.jpg" alt="">
+					<br>
+					<p>Se perdió en  villa lago gutierrez llamar al 2944920440</p>
+						<div class="card-body">
+							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+						</div>
+				</div>
+      </article>
+
+			<article>
+				<div class="card text-center alert alert-primary">
+					<img class="" src="images/perdido4.jpg" alt="">
+					<br>
+					<p>Está en la zona de gallardo y Rivadavia. Se nota que está perdido!!</p>
+						<div class="card-body">
+							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+						</div>
+				</div>
+      </article>
+
+			<article>
+				<div class="card text-center alert alert-primary">
+					<img class="" src="images/perdido5.jpg" alt="">
+					<br>
+					<p>Se extravió caniche macho sin cola con collar azul en San Francisco 3 agradecere info</p>
+						<div class="card-body">
+							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+						</div>
+				</div>
+			</article>
+
+			<article>
+				<div class="card text-center alert alert-primary">
+					<img class="" src="images/perdido6.jpg" alt="">
+					<br>
+					<p>Está este perro en la entrada a las victorias con colla verde</p>
+						<div class="card-body">
+							<a href="#" class="btn btn-primary alert alert-primary">Contactar Dueño</a>
+						</div>
+				</div>
+			</article>
+		</div>
+</section>
