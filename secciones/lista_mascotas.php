b58cba7ab2a17dbf4971979fa1c59878a75c8e6b
Lautaro <lauthuichalaf@gmail.com>
2018-11-23 10:39:50 -0300

    Junté los dos modales para perdidos y encontrados. También borró comentarios innecesarios

--- secciones/lista_mascotas.php
+++ secciones/lista_mascotas.php
@@ -4,16 +4,13 @@ session_start();
 include "../assets/config.php";
 
 
-$sql="SELECT `tipo`, `publicacion`, `fecha`, `id_imagen` FROM `publicaciones` WHERE (tipo=0 OR tipo=1);";
+$sql="SELECT `tipo`, `publicacion`, `fecha`, `id_imagen` FROM `publicaciones` INNER JOIN  imagenes WHERE (tipo='0' OR tipo='1');";
 
 $query= mysqli_query($conexion, $sql);
 //  echo $sql;
 while ($fila=mysqli_fetch_array($query)){
 
-
-
-
-  $img="images/perdido3.jpg";//$fila["urlimg"];
+  $img=$fila["imagenes"];//$fila["urlimg"];
   $tipo=$fila["tipo"];
   $mensaje=$fila["publicacion"];
   $fecha=$fila["fecha"];
