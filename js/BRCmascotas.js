dcfcc436c3a1072b41321e0450be0aa95e20a450
Lautaro <lauthuichalaf@gmail.com>
2018-11-30 10:26:37 -0300

    Dejé las cosas que se usan en las páginas en común, creé otros js que son de secciones expecificas

--- js/BRCmascotas.js
+++ js/BRCmascotas.js
@@ -43,75 +43,34 @@ jQuery(document).ready(function(){
             })
         }
 
-    //MODAL QUE SIRVE PARA LA PUBLICACION DE PERROS PERDIDOS Y ENCONTRADOS (LO SUBE A LA BASE DE DATOS)
-      $("#contenedor_seccion").on("click", "#publicar", function(){
-        // var datos={
-        //   edad:25,
-        //   nombre:"Laura",
-        // }
-        var formData = new FormData();
-        var file = $("#id_foto")[0].files[0];
-        //  formData.append("username", "Groucho");
-
-          // HTML file input, chosen by user
-          formData.append("foto", file);
-
-        $.ajax({
-          //async: true,
-          data: formData,
-          cache: false,
-          contentType: false,
-          processData: false,
-          url: "assets/up_foto.php",
-          type: "POST",
-          success: function(id_foto){
-            var datos = $("#form_perdidos").serialize();
-            datos = datos+"&id_foto="+ id_foto;
-            $.ajax({
-              type:"post",
-              data:datos,
-              url:"assets/ABM_publicacion.php",
-              success: function(resultado){
-                alert("Publicación cargada con éxito");
-                $("#modal_encontrado_perdido").modal("hide");
-                traerlistamascotas();
-              }
-
-            })
-
-            alert("Publicación cargada con éxito");
-          }
-        });
 
 
-      })
+//       function traerlistaadopcion() {
+//         $.ajax({
+//           type:"post",
+//           url:"secciones/lista_adopcion.php",
+//           success: function (resultado) {
+//             $("#lista_adopcion").html(resultado);
+// }
+//         })
+//       }
 
-      //SUBE LA TARJETA A LA SECCIÓN
-      function traerlistamascotas(){
-          $.ajax({
-            type:"post",
-            url:"secciones/lista_mascotas.php",
-            success: function (resultado) {
-              $("#lista_mascotas").html(resultado);
-}
-          })
-      }
 
-//ABRE EL MODAL
 
-$("#contenedor_seccion").on("click", ".btn_encontrado_perdido", function(){
-  var tipo=$(this).attr("tipo");//0 o 1
 
-    $("#txt_tipo").val(tipo);
-    $("#txt_titulo").text("Perro Encontrado");
-    if (tipo==0)  {
-      $("#txt_titulo").text("Perro Perdido");
-    }
-
-  $("#modal_encontrado_perdido").modal("show");
-
-});
-
+//ABRE EL MODAL ADopcion
+// $("#contenedor_seccion").on("click", ".btn_adopcion", function(){
+//   var tipo=$(this).attr("tipo");//0 o 1
+//
+//     $("#txt_tipo").val(tipo); //tendría que ser 3 (si esto funciona) por ende amarillo
+//     $("#txt_titulo").text("Perro en adopcion");
+//     if (tipo==3)  {
+//       $("#txt_titulo").text("Perro Perdido");
+//     }
+//
+//   $("#modal_adopcion").modal("show");
+//
+// });
 
   //
   // function traertarjetas(){
