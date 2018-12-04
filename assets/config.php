5928aea2a4ef95b05e2a1cbf466cb49f4e67deab
Lautaro <lauthuichalaf@gmail.com>
2018-12-04 14:03:37 -0300

    Eliminé donde definía el id del usuario

--- assets/config.php
+++ assets/config.php
@@ -5,5 +5,5 @@ mysqli_set_charset($conexion,"utf8");
 date_default_timezone_set("America/Argentina/Buenos_Aires");
 
 
-$_SESSION["id_usuario"]=2; //temporal, cuando cree el login esto se borra.
+//$_SESSION["id_usuario"]=2; //temporal, cuando cree el login esto se borra.
 ?>
