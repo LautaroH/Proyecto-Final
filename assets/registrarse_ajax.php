7737a0dffd76dc895728898af670b5c72fcf5fcc
Lautaro <lauthuichalaf@gmail.com>
2018-12-09 14:30:34 -0300

    Creé que se pueda registar un usuario.

---
+++ assets/registrarse_ajax.php
@@ -0,0 +1,21 @@
+<?php
+session_name("BRCMascotas");
+session_start();
+include "config.php";
+//$_SESSION["id_usuario"]=2; //temporal, cuando cree el login esto se borra.
+
+$usuario=$_POST['mail']; // El usuario se logea con el mail
+$nombre=$_POST['usuario']; //es el nombre del usuario.
+$clave=$_POST['clave'];	//
+//$clave=md5($clave);
+$resultado="Falso";
+
+//INSERT INTO `usuarios`(`usuario`, `clave`, `mail`) VALUES ('Carlos', 'carlos123', 'carlos@gmail.com')
+$sql= "INSERT INTO `usuarios`(`usuario`, `clave`, `mail`)  VALUES ('$nombre', '$clave', '$usuario')";
+		$query= mysqli_query($conexion, $sql);
+			//  echo $sql;
+echo $resultado;
+//echo $_SESSION["id_usuario"];
+
+
+?>
