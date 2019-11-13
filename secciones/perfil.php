6f7433724d4b54c4e4d8805f741c5313b81fc0a5
Haytam95 <ivanpgn87@gmail.com>
2019-11-13 18:50:55 -0300

    Identificadas partes del chat

--- secciones/perfil.php
+++ secciones/perfil.php
@@ -181,6 +181,8 @@ session_start();
                         </span> </div>
                     </div>
                   </div>
+
+                  <!-- Listado de personas con las que se tienen mensajes. -->
                   <div class="inbox_chat">
                     <div class="chat_list active_chat">
                       <div class="chat_people">
@@ -192,109 +194,34 @@ session_start();
                         </div>
                       </div>
                     </div>
-                    <!-- <div class="chat_list">
-                      <div class="chat_people">
-                        <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                        <div class="chat_ib">
-                          <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions
-                            astrology under one roof.</p>
-                        </div>
-                      </div>
-                    </div> -->
-                    <!-- <div class="chat_list">
-                      <div class="chat_people">
-                        <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                        <div class="chat_ib">
-                          <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions
-                            astrology under one roof.</p>
-                        </div>
-                      </div>
-                    </div> -->
-                    <!-- <div class="chat_list">
-                      <div class="chat_people">
-                        <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                        <div class="chat_ib">
-                          <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions
-                            astrology under one roof.</p>
-                        </div>
-                      </div>
-                    </div> -->
-                    <!-- <div class="chat_list">
-                      <div class="chat_people">
-                        <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                        <div class="chat_ib">
-                          <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions
-                            astrology under one roof.</p>
-                        </div>
-                      </div>
-                    </div> -->
-                    <!-- <div class="chat_list">
-                      <div class="chat_people">
-                        <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                        <div class="chat_ib">
-                          <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions
-                            astrology under one roof.</p>
-                        </div>
-                      </div>
-                    </div> -->
-                    <!-- <div class="chat_list">
-                      <div class="chat_people">
-                        <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                        <div class="chat_ib">
-                          <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                          <p>Test, which is a new approach to have all solutions
-                            astrology under one roof.</p>
-                        </div>
-                      </div>
-                    </div> -->
                   </div>
+
+
                 </div>
                 <div class="mesgs">
                   <div class="msg_history">
                     <div class="incoming_msg">
                       <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
                       <div class="received_msg">
+                        
+                        <!-- Mensaje entrante -->
                         <div class="received_withd_msg">
                           <p>Test which is a new approach to have all
                             solutions</p>
-                          <span class="time_date"> </span></div>
+                          <span class="time_date">asdasdasdasd</span>
+                        </div>
                       </div>
                     </div>
+
+                    <!-- Mensaje saliente -->
                     <div class="outgoing_msg">
                       <div class="sent_msg">
                         <p>Test which is a new approach to have all
                           solutions</p>
                         <span class="time_date"> </span> </div>
                     </div>
-                    <div class="incoming_msg">
-                      <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                      <div class="received_msg">
-                        <div class="received_withd_msg">
-                          <p>Test, which is a new approach to have</p>
-                          <span class="time_date"> </span></div>
-                      </div>
-                    </div>
-                    <div class="outgoing_msg">
-                      <div class="sent_msg">
-                        <p>Apollo University, Delhi, India Test</p>
-                        <span class="time_date"> </span> </div>
-                    </div>
-                    <div class="incoming_msg">
-                      <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                      <div class="received_msg">
-                        <div class="received_withd_msg">
-                          <p>We work directly with our designers and suppliers,
-                            and sell direct to you, which means quality, exclusive
-                            products, at a price anyone can afford.</p>
-                          </div>
-                      </div>
-                    </div>
                   </div>
+
                   <div class="type_msg">
                     <div class="input_msg_write">
                       <input type="text" class="write_msg" placeholder="Type a message" />
@@ -310,19 +237,8 @@ session_start();
             </div></div>
             </body>
             </html>
-        <script type="text/javascript">
-
-        </script>
         </body>
         </html>
-
-				<div id="ourserv">
-				</div>
-				<div id="sline">
-					<div class="sdline"></div>
-
-					<div class="sdline"></div>
-				</div>
 
 			</section>
 			<!--__--__--__--__--  T H E    F O O T E R --__--__--__--___--__--__--__-->
