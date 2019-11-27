3ec13101efe219a4033dd00df50ba5c35c6dd0a0
Haytam95 <ivanpgn87@gmail.com>
2019-11-27 19:57:04 -0300

    Agregada protección para no poder contactar usuario si la publicación es tuya

--- secciones/lista_adopcion.php
+++ secciones/lista_adopcion.php
@@ -3,6 +3,11 @@ session_name("BRCMascotas");
 session_start();
 include "../assets/config.php";
 
+$idUsuarioActual = null;
+if(isset($_SESSION['id_usuario'])) {
+  $idUsuarioActual = $_SESSION['id_usuario'];
+}
+
 
 $sql="SELECT p.`id`, p.`tipo`, p.`id_usuario`, p.`publicacion`, p.`fecha`, i.`imagen` FROM `publicaciones` p LEFT JOIN `imagenes` i ON (`i`.`id`=`p`.`id_imagen`) WHERE (`p`.`tipo`='3');";
 //"SELECT `tipo`, `publicacion`, `fecha`, `id_imagen` FROM `publicaciones` INNER JOIN `imagenes` ON `imagenes`.`id`=`publicaciones`.`id_imagen` WHERE (tipo='0' OR tipo='1');";
@@ -41,7 +46,23 @@ while ($fila=mysqli_fetch_array($query)){
    <br>
    <p> <?=$mensaje?> <?=$fecha?> </p>
      <div class="card-body">
-       <a href="#" class="btn btn-<?=$tipo?> alert alert-light iniciar_chat_usuario" id_usuario="<?php echo $id_usuario; ?>" id_publicacion="<?php echo $id; ?>">Contactar Usuario</a>
+
+      <?php
+      
+      if($idUsuarioActual == $id_usuario) {
+        ?>
+        <a href="#" class="btn btn-<?=$tipo?> alert alert-light">Ésta publicación es tuya</a>
+
+      <?php
+      } else {
+
+      ?>
+        <a href="#" class="btn btn-<?=$tipo?> alert alert-light iniciar_chat_usuario" id_usuario="<?php echo $id_usuario; ?>" id_publicacion="<?php echo $id; ?>">Contactar Usuario</a>
+      <?php
+      }
+
+      ?>
+
      </div>
  </div>
 </article>
