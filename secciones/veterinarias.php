570d7fa6b1ed1e7e692f2b2be38be21ec1a95e4d
Lautaro <lauthuichalaf@gmail.com>
2019-12-08 14:33:05 -0300

    Hice responsive el mapa de veterianarias

--- secciones/veterinarias.php
+++ secciones/veterinarias.php
@@ -11,7 +11,28 @@ session_start();
 				</div>
         <div id="ourserv">
 
-        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d48080.03442541125!2d-71.3512941594824!3d-41.13447899520376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sveterinarias!5e0!3m2!1ses!2sar!4v1539560754087" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+          <style>
+    .google-maps {
+    position: relative;
+    padding-bottom: 75%; // This is the aspect ratio
+    height: 0;
+    overflow: hidden;
+    }
+    .google-maps iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100% !important;
+    height: 100% !important;
+    }
+    </style>
+
+    <div class="google-maps">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d48080.03442541125!2d-71.3512941594824!3d-41.13447899520376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sveterinarias!5e0!3m2!1ses!2sar!4v1539560754087" width="600" height="450" frameborder="0" style="border:0"></iframe>
+    </div>
+
+      <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d48080.03442541125!2d-71.3512941594824!3d-41.13447899520376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sveterinarias!5e0!3m2!1ses!2sar!4v1539560754087" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
 
         </div>
 			</section>
