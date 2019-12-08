3f227dbb0d59c8daef8ff74e3afda777008c7582
Lautaro <lauthuichalaf@gmail.com>
2019-12-08 15:22:12 -0300

    Cambie el orden

--- secciones/mensajes.php
+++ secciones/mensajes.php
@@ -1,10 +1,203 @@
 <?php
+    session_name("BRCMascotas");
+
     require_once("../entidades/usuario.php");
     require_once("../entidades/chat.php");
     require_once("../assets/functions.php");
  ?>
 
+ <body>
 
+     <div class="container">
+         <!-- <h3 class=" text-center">Messaging</h3> -->
+         <div id="line">
+           <div class="dline"></div>
+           <h1>Mensajes</h1>
+           <div class="dline"></div>
+         </div>
+         <div class="messaging">
+             <div class="inbox_msg">
+                 <div class="inbox_people">
+                     <div class="headind_srch">
+                         <div class="recent_heading">
+                             <h4>Recent</h4>
+                         </div>
+                         <div class="srch_bar">
+                             <div class="stylish-input-group"></div>
+                         </div>
+                     </div>
+
+                     <!-- Listado de personas con las que se tienen mensajes. -->
+                     <div class="inbox_chat">
+                         <div class="chat_list active_chat">
+                             <div class="chat_people">
+                                 <?php
+
+                                 $usuariosList = listUsuarioMessage();
+
+                                 foreach ($usuariosList as $usuario) {
+
+                                     $ultimoMensaje = findLastMessage($usuario->id);
+                                     $ultimoMensajeText = "(El usuario no envió mensajes)";
+
+                                     if($ultimoMensaje != null) {
+                                         $ultimoMensajeText = $ultimoMensaje->mensaje;
+                                     }
+
+                             ?>
+                                 <a href="#" class="usuarioChat" <?php echo 'id_usuario="'.$usuario->id.'"'; ?>>
+                                     <div class="chat_ib">
+                                         <h5><?php echo $usuario->usuario ?><span class="chat_date"></span>
+                                         </h5>
+                                         <p><?php echo $ultimoMensajeText; ?></p>
+                                     </div>
+                                 </a>
+                                 <?php
+                                 }
+                             ?>
+                             </div>
+                         </div>
+                     </div>
+
+                 </div>
+                 <div class="mesgs">
+                     <div id="msg_history" class="msg_history">
+                         <div class="incoming_msg">
+                             <!-- Mensaje entrante -->
+                             <div class="received_msg">
+                                 <div class="received_withd_msg">
+                                     <p>Para conversar con un usuario pulsá sobre su nombre</p>
+                                     <span class="time_date"></span>
+                                 </div>
+                             </div>
+                         </div>
+
+                         <!-- Mensaje saliente -->
+                         <div class="outgoing_msg">
+                             <div class="sent_msg">
+                                 <p>Para iniciar una conversación nueva pulsá "Contactar" sobre un anuncio</p>
+                                 <span class="time_date"> </span>
+                             </div>
+                         </div>
+                     </div>
+
+                     <div class="type_msg">
+                         <div class="input_msg_write">
+                             <input id="messageText" type="text" class="write_msg" placeholder="Type a message" />
+
+                             <button id="btnSendMessage" class="msg_send_btn" type="button"><i
+                                     class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+
+     </html>
+
+     <script>
+
+         var usuarioChatActual = null;
+
+         $("#btnSendMessage").on("click", function () {
+             sendMessage();
+         });
+
+         $(".usuarioChat").on("click", function () {
+             chatWith($(this).attr("id_usuario"));
+         })
+
+
+         function chatWith(idUsuario) {
+             usuarioChatActual = idUsuario;
+
+             $("#messageText").val("");
+             updateMessagesBody(idUsuario);
+         }
+
+         function sendMessage() {
+
+
+
+             let texto = $("#messageText").val();
+             if(!usuarioChatActual) return alert("No hay ningún usuario seleccionado para chatear.");
+             if (!texto) return alert("Debes escribir el texto a enviar.");
+
+             $.ajax({
+                 url: 'assets/sendMessage.php',
+                 data: "idDestinatario="+usuarioChatActual+"&mensaje="+texto+"&idPublicacion=0",
+                 method: 'POST',
+                 success: function(data) {
+                     updateMessagesBody(usuarioChatActual);
+                 },
+                 error: function(error) {
+                     alert(error);
+                 }
+             })
+
+
+             $("#messageText").val("");
+         }
+
+
+         setInterval(function () {
+             if(usuarioChatActual != null) {
+                 updateMessagesBody(usuarioChatActual);
+             }
+         }, 5000);
+
+         function updateMessagesBody(idUsuario) {
+
+             $.ajax({
+                 url: 'assets/listMensaje.php?idUsuarioRemitente=' + idUsuario,
+                 method: 'GET',
+                 success: function (data) {
+
+                     $("#msg_history").html("");
+
+                     var html = "";
+
+                     var lista = JSON.parse(data);
+
+                     for (var i = 0; i < lista.length; i++) {
+
+                         if (lista[i].id_usuario == <?php echo $_SESSION['id_usuario']; ?>) {
+
+                             html += "<div class=\"outgoing_msg\">"+
+                                         "<div class=\"sent_msg\">"+
+                                             "<p>"+lista[i].mensaje+"</p>"+
+                                             "<span class=\"time_date\">"+lista[i].fecha+"</span>"+
+                                         "</div>"+
+                                     "</div>";
+
+                         } else {
+
+                             html += "<div class=\"incoming_msg\">"+
+                             "<div class=\"received_msg\">"+
+                                 "<div class=\"received_withd_msg\">"+
+                                     "<p>"+lista[i].mensaje+"</p>"+
+                                     "<span class=\"time_date\">"+lista[i].fecha+"</span>"+
+                                     "</div>"+
+                                 "</div>"+
+                             "</div>";
+                         }
+                     }
+
+                     $("#msg_history").html(html);
+                 }
+             });
+
+         }
+
+
+
+     </script>
+
+
+
+
+<!-- CSS -->
 <head>
     <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css"
@@ -228,191 +421,3 @@
     <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 </head>
-
-<body>
-
-    <div class="container">
-        <!-- <h3 class=" text-center">Messaging</h3> -->
-        <div id="line">
-          <div class="dline"></div>
-          <h1>Mensajes</h1>
-          <div class="dline"></div>
-        </div>
-        <div class="messaging">
-            <div class="inbox_msg">
-                <div class="inbox_people">
-                    <div class="headind_srch">
-                        <div class="recent_heading">
-                            <h4>Recent</h4>
-                        </div>
-                        <div class="srch_bar">
-                            <div class="stylish-input-group"></div>
-                        </div>
-                    </div>
-
-                    <!-- Listado de personas con las que se tienen mensajes. -->
-                    <div class="inbox_chat">
-                        <div class="chat_list active_chat">
-                            <div class="chat_people">
-                                <?php
-
-                                $usuariosList = listUsuarioMessage();
-
-                                foreach ($usuariosList as $usuario) {
-
-                                    $ultimoMensaje = findLastMessage($usuario->id);
-                                    $ultimoMensajeText = "(El usuario no envió mensajes)";
-
-                                    if($ultimoMensaje != null) {
-                                        $ultimoMensajeText = $ultimoMensaje->mensaje;
-                                    }
-
-                            ?>
-                                <a href="#" class="usuarioChat" <?php echo 'id_usuario="'.$usuario->id.'"'; ?>>
-                                    <div class="chat_ib">
-                                        <h5><?php echo $usuario->usuario ?><span class="chat_date"></span>
-                                        </h5>
-                                        <p><?php echo $ultimoMensajeText; ?></p>
-                                    </div>
-                                </a>
-                                <?php
-                                }
-                            ?>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="mesgs">
-                    <div id="msg_history" class="msg_history">
-                        <div class="incoming_msg">
-                            <!-- Mensaje entrante -->
-                            <div class="received_msg">
-                                <div class="received_withd_msg">
-                                    <p>Para conversar con un usuario pulsá sobre su nombre</p>
-                                    <span class="time_date"></span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Mensaje saliente -->
-                        <div class="outgoing_msg">
-                            <div class="sent_msg">
-                                <p>Para iniciar una conversación nueva pulsá "Contactar" sobre un anuncio</p>
-                                <span class="time_date"> </span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="type_msg">
-                        <div class="input_msg_write">
-                            <input id="messageText" type="text" class="write_msg" placeholder="Type a message" />
-
-                            <button id="btnSendMessage" class="msg_send_btn" type="button"><i
-                                    class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    </html>
-
-    <script>
-
-        var usuarioChatActual = null;
-
-        $("#btnSendMessage").on("click", function () {
-            sendMessage();
-        });
-
-        $(".usuarioChat").on("click", function () {
-            chatWith($(this).attr("id_usuario"));
-        })
-
-
-        function chatWith(idUsuario) {
-            usuarioChatActual = idUsuario;
-
-            $("#messageText").val("");
-            updateMessagesBody(idUsuario);
-        }
-
-        function sendMessage() {
-
-
-
-            let texto = $("#messageText").val();
-            if(!usuarioChatActual) return alert("No hay ningún usuario seleccionado para chatear.");
-            if (!texto) return alert("Debes escribir el texto a enviar.");
-
-            $.ajax({
-                url: 'assets/sendMessage.php',
-                data: "idDestinatario="+usuarioChatActual+"&mensaje="+texto+"&idPublicacion=0",
-                method: 'POST',
-                success: function(data) {
-                    updateMessagesBody(usuarioChatActual);
-                },
-                error: function(error) {
-                    alert(error);
-                }
-            })
-
-
-            $("#messageText").val("");
-        }
-
-
-        setInterval(function () {
-            if(usuarioChatActual != null) {
-                updateMessagesBody(usuarioChatActual);
-            }
-        }, 5000);
-
-        function updateMessagesBody(idUsuario) {
-
-            $.ajax({
-                url: 'assets/listMensaje.php?idUsuarioRemitente=' + idUsuario,
-                method: 'GET',
-                success: function (data) {
-
-                    $("#msg_history").html("");
-
-                    var html = "";
-
-                    var lista = JSON.parse(data);
-
-                    for (var i = 0; i < lista.length; i++) {
-
-                        if (lista[i].id_usuario == <?php echo $_SESSION['id_usuario']; ?>) {
-
-                            html += "<div class=\"outgoing_msg\">"+
-                                        "<div class=\"sent_msg\">"+
-                                            "<p>"+lista[i].mensaje+"</p>"+
-                                            "<span class=\"time_date\">"+lista[i].fecha+"</span>"+
-                                        "</div>"+
-                                    "</div>";
-
-                        } else {
-
-                            html += "<div class=\"incoming_msg\">"+
-                            "<div class=\"received_msg\">"+
-                                "<div class=\"received_withd_msg\">"+
-                                    "<p>"+lista[i].mensaje+"</p>"+
-                                    "<span class=\"time_date\">"+lista[i].fecha+"</span>"+
-                                    "</div>"+
-                                "</div>"+
-                            "</div>";
-                        }
-                    }
-
-                    $("#msg_history").html(html);
-                }
-            });
-
-        }
-
-
-
-    </script>
